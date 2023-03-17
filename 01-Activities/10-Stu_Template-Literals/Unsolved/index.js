902f03afa028df6347d2ffd1570cdb047eb918d9
Gerry Paul <gpaul12@nc.rr.com>
2023-03-16 21:34:33 -0400

    10-Stu

--- 01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,12 @@
 // music should be an object with title, artist, and album properties
 const music = {
   // code here
- 
+  title: "Yeke Yeke",
+  artist: "Mory Kante",
+  album: "Akwaba Beach",
 };
 
 // Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+const songSnippet = `${music.title} by ${music.artist} from the album ${music.album}`;
 
-console.log(songSnippet)
-
+console.log(songSnippet);
