26303a0e09aaedb86023c42dbaf6feb783184a0f
Gerry Paul <gpaul12@nc.rr.com>
2023-03-21 18:58:03 -0400

    21-22

--- 01-Activities/21-Ins_for-of/index.js
+++ 01-Activities/21-Ins_for-of/index.js
@@ -1,6 +1,16 @@
 // 1. Using arrays
-const songs = ['Bad Guy', 'The Wheels on the Bus', 'Friday'];
+const songs = ["Bad Guy", "The Wheels on the Bus", "Friday"];
 
+console.log("--- for loop");
+for (let i = 0; i < songs.length; i++) {
+  let song = songs[i];
+  console.log(song);
+}
+
+console.log("--- forEach()");
+songs.forEach((song) => console.log(song));
+
+console.log("--- for...of");
 for (const song of songs) {
   console.log(song);
 }
@@ -11,19 +21,19 @@ for (const song of songs) {
   Friday
 */
 
-console.log('\n================================================\n');
+// console.log("\n================================================\n");
 
 // 2. Using multidimensional arrays
-const moreSongs = [
-  ['Bad Guy', 1],
-  ['The Wheels on the Bus', 2],
-  ['Friday', 3],
-];
+// const moreSongs = [
+//   ["Bad Guy", 1],
+//   ["The Wheels on the Bus", 2],
+//   ["Friday", 3],
+// ];
 
-// for of loop
-for (const song of moreSongs) {
-  console.log(song);
-}
+// // for of loop
+// for (const song of moreSongs) {
+//   console.log(song);
+// }
 
 /* Output:
   Bad Guy's chart position is 1
